ae82df9ca1788cd97f9325b0af690d18ba0efcba
Shivam Tripathi <tripathi.shivam28@gmail.com>
2024-05-22 19:01:28 +0530

    update

--- extract_keyframe.py
+++ extract_keyframe.py
@@ -1,7 +1,12 @@
+import os
 import cv2
 import librosa
+from tqdm import tqdm
 from PIL import Image
 import soundfile as sf
+
+import warnings
+warnings.filterwarnings("ignore")
 
 import torch
 import torch.nn.functional as F
@@ -11,7 +16,6 @@ from network import AVModel
 from soundnet import SoundNet
 
 
-SR = 22000
 mean = [0.485, 0.456, 0.406]
 std = [0.229, 0.224, 0.225]
 image_transforms = transforms.Compose([transforms.Resize((320, 320)),   # AVModel expects 320x320 input
@@ -34,15 +38,31 @@ def extract_frames(video_path):
     return frames
 
 
-def get_keyframe(device, model, frames, audio):
+def process_video(video_file, device, model, soundnet):
 
+    # extract audio
+    audio, _ = librosa.load(video_file, sr=22000, mono=True)     # audio = (220172,), 22kHz used in SoundNet
+
+    # preprocess audio for soundnet
+    audio_input = audio * (2 ** -23)
+    audio_input = torch.from_numpy(audio_input).float().view(1, 1, -1, 1)   # batch, channels, length, extra = 1, 1, 220172, 1
+    audio_input = audio_input.to(device)
+    
+    # AVModel uses the output features of "object" branch of Conv8 layer from SoundNet model 
+    audio_soundnet_features = soundnet(audio_input)     # 1x1000x1x1
+    audio_soundnet_features = audio_soundnet_features.squeeze().view(1, -1)   # 1x1000
+
+    # frames=250x3x320x320, # audio=(22012,)
+    frames = extract_frames(video_file)
+
+    # get key_frame
     highest_corr = float('-inf')
     key_frame = None
     frame_idx = None
 
     # process audio embedding through AudioNet
     with torch.no_grad():
-        audio_embedding = model.audio_embedding_model(audio)   
+        audio_embedding = model.audio_embedding_model(audio_soundnet_features)   
     
         # compute audio embedding and attention map
         h = model.sound_fc1(audio_embedding)
@@ -87,12 +107,18 @@ def get_keyframe(device, model, frames, audio):
 
     # print(f"{frame_idx+1} frame selected out of 250 frames")
 
-    return key_frame
+    return key_frame, audio
 
 
 if __name__ == "__main__":
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    video_folder = '/shika_backup/data3/shivam/audio-visual-dataset/unbalanced_train_1'
+    save_at = '/shika_backup/data3/shivam/audioset/'
+    os.makedirs(save_at, exist_ok=True)
+    os.makedirs(os.path.join(save_at, 'images'), exist_ok=True)
+    os.makedirs(os.path.join(save_at, 'audio'), exist_ok=True)
 
     model = AVModel().to(device).eval()
     model.load_state_dict(torch.load("sound_localization_latest.pth"))
@@ -100,25 +126,25 @@ if __name__ == "__main__":
     soundnet = SoundNet().to(device).eval()
     soundnet.load_state_dict(torch.load("soundnet8_final.pth"))
 
-    video_path = 'video_samples/-0sDdBQt3Gc_"Railroad car, train wagon, Rail transport, Train, Vehicle".mp4'
-    
-    # frames=250x3x320x320, # audio=(22012,)
-    frames= extract_frames(video_path)
-    # extract audio
-    audio, sr = librosa.load(video_path, sr=22000, mono=True)     # audio = (220172,), 22kHz used in SoundNet
+    error_count = 0
+    pbar = tqdm(total=len(os.listdir(video_folder)), initial=0, desc="Processing")
+    for video_file in os.listdir(video_folder):
+        if video_file.endswith(".mp4"):
+            try:
+                key_frame, audio = process_video(os.path.join(video_folder, video_file),
+                                                 device,
+                                                 model,
+                                                 soundnet)
+        
+                # save key-frame and audio
+                video_basename = os.path.basename(video_file)
+                video_name = os.path.splitext(video_basename)[0]
+                key_frame.save(os.path.join(save_at, "images", f"{video_name}.png"))
+                sf.write(os.path.join(save_at, "audio", f"{video_name}.wav"), audio, 22000)
+            except Exception as e:
+                print("Error in processing video: ", e)
+                error_count += 1
+        pbar.set_description(f"Errors: {error_count}")
+        pbar.update(1)
 
-    # preprocess audio for soundnet
-    audio_input = audio * (2 ** -23)
-    audio_input = torch.from_numpy(audio_input).float().view(1, 1, -1, 1)   # batch, channels, length, extra = 1, 1, 220172, 1
-    audio_input = audio_input.to(device)
-    
-    # AVModel uses the output features of "object" branch of Conv8 layer from SoundNet model 
-    audio_soundnet_features = soundnet(audio_input)     # 1x1000x1x1
-    audio_soundnet_features = audio_soundnet_features.squeeze().view(1, -1)   # 1x1000
-
-    key_frame_image = get_keyframe(device, model, frames, audio_soundnet_features)
-    
-    # save key-frame and audio
-    key_frame_image.save("keyframe.png")
-    sf.write("audio.wav", audio, 22000)
-    
+    print(f"Total number of files: {len(os.listdir(video_folder))}, and {error_count} files could not be processed")
