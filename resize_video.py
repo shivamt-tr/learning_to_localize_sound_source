f8ee871a70e23f9a57a04d8e9995b56f582f5c2d
Shivam Tripathi <tripathi.shivam28@gmail.com>
2024-05-27 02:06:57 +0530

    update

--- resize_video.py
+++ resize_video.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import imageio
 import librosa
 import argparse
 import numpy as np
@@ -14,8 +15,10 @@ sample_rate = 22050  # for SoundNet model input
 target_length = 220672
 
 
-def process_video(video_file, args):
-    video_path = os.path.join(args.data_dir, video_file)
+def process_video(args):
+
+    video_file, data_dir, save_dir = args
+    video_path = os.path.join(data_dir, video_file)
     video_name = os.path.splitext(os.path.basename(video_path))[0]
 
     try:
@@ -25,7 +28,7 @@ def process_video(video_file, args):
             raise IOError(f"Error opening video file: {video_path}")
 
         fps = cap.get(cv2.CAP_PROP_FPS)
-        writer = imageio.get_writer(os.path.join(args.save_dir, "resized_video", f"{video_name}.mp4"), mode='I', fps=fps)
+        writer = imageio.get_writer(os.path.join(save_dir, "resized_video", f"{video_name}.mp4"), mode='I', fps=fps)
 
         while True:
             ret, frame = cap.read()
@@ -47,7 +50,7 @@ def process_video(video_file, args):
         if audio.shape[0] < target_length:
             audio = np.pad(audio, (0, target_length - audio.shape[0]), 'constant')
 
-        sf.write(os.path.join(args.save_dir, "audio", f"{video_name}.wav"), audio, sample_rate)
+        sf.write(os.path.join(save_dir, "audio", f"{video_name}.wav"), audio, sample_rate)
     except Exception as e:
         return f"Error in processing video: {video_path}, Error: {e}"
 
@@ -60,9 +63,10 @@ def main(args):
     os.makedirs(os.path.join(args.save_dir, 'audio'), exist_ok=True)
 
     video_files = [f for f in os.listdir(args.data_dir) if f.endswith(".mp4")]
+    pool_args = [(video_file, args.data_dir, args.save_dir) for video_file in video_files]
 
     with Pool(processes=args.num_workers) as pool:
-        results = list(tqdm(pool.imap(lambda video_file: process_video(video_file, args), video_files), total=len(video_files), desc="Processing"))
+        results = list(tqdm(pool.imap(process_video, pool_args), total=len(video_files), desc="Processing"))
 
     error_count = sum(1 for result in results if result is not None)
     for result in results:
