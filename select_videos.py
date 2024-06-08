a08f94ce1c4016d76dba939ebc433d937483b7c2
Shivam Tripathi <tripathi.shivam28@gmail.com>
2024-06-08 19:27:56 +0530

    update

--- select_videos.py
+++ select_videos.py
@@ -7,7 +7,7 @@ def copy_random_videos(src_folder, dest_folder, num_videos=50):
     os.makedirs(dest_folder, exist_ok=True)
 
     # List all mp4 files in the source folder
-    video_files = [f for f in os.listdir(src_folder) if f.endswith('.mp4')]
+    video_files = [f for f in os.listdir(src_folder) if f.endswith('.png')]
 
     # Check if there are enough videos in the source folder
     if len(video_files) < num_videos:
@@ -23,7 +23,25 @@ def copy_random_videos(src_folder, dest_folder, num_videos=50):
         shutil.copy(src_path, dest_path)
         print(f"Copied {video} to {dest_folder}")
 
+
+def copy_files_with_keyword(source_folder, destination_folder, keyword):
+    # Create destination folder if it doesn't exist
+    os.makedirs(destination_folder, exist_ok=True)
+    
+    # Iterate over all files in the source folder
+    for file_name in os.listdir(source_folder):
+        # Check if the keyword is in the file name
+        if keyword in file_name:
+            source_path = os.path.join(source_folder, file_name)
+            destination_path = os.path.join(destination_folder, file_name)
+            
+            # Copy the file to the destination folder
+            shutil.copy(source_path, destination_path)
+            print(f"Copied: {file_name}")
+
+
 if __name__ == "__main__":
-    src_folder = "/shika_backup/data3/shivam/audio-visual-dataset/unbalanced_train_4/"
-    dest_folder = "/shika_data4/shivam/video_samples"
-    copy_random_videos(src_folder, dest_folder, num_videos=50)
+    src_folder = "/data4/shivam/dataset/vggsound_processed/images"
+    dest_folder = "/data4/shivam/samples_vggsound"
+    # copy_random_videos(src_folder, dest_folder, num_videos=1000)
+    copy_files_with_keyword(src_folder, dest_folder, keyword="sobbing")
